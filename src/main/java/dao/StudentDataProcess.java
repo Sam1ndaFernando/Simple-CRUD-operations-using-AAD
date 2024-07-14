d1a602ea2942e01246f9ddb982886fc7d2b58a29
Sam1ndaFernando <uthpala2001@gmail.com>
2024-07-14 11:12:01 +0530

    queries added

--- src/main/java/dao/StudentDataProcess.java
+++ src/main/java/dao/StudentDataProcess.java
@@ -5,6 +5,11 @@ import dto.StudentDto;
 import java.sql.Connection;
 
 public final class StudentDataProcess implements StudentData {
+    static String SAVE_STUDENT = "INSERT INTO students (id, name, email, city, level) VALUES (?, ?, ?, ?, ?)";
+    static String GET_STUDENT = "SELECT * FROM students WHERE id=?";
+    static String DELETE_STUDENT = "DELETE FROM students WHERE id=?";
+    static String UPDATE_STUDENT = "UPDATE students SET name=?, email=?, city=?, level=? WHERE id=?";
+
     @Override
     public StudentDto getStudent(String StudentId, Connection connection) {
         return null;
