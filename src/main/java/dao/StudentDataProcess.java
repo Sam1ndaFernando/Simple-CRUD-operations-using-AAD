821844aa5b86ca638f395884562d0f7d524cbe9f
Sam1ndaFernando <uthpala2001@gmail.com>
2024-07-15 19:40:23 +0530

    getStudent method complete

--- src/main/java/dao/StudentDataProcess.java
+++ src/main/java/dao/StudentDataProcess.java
@@ -3,6 +3,9 @@ package dao;
 import dto.StudentDto;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public final class StudentDataProcess implements StudentData {
     static String SAVE_STUDENT = "INSERT INTO students (id, name, email, city, level) VALUES (?, ?, ?, ?, ?)";
@@ -12,9 +15,25 @@ public final class StudentDataProcess implements StudentData {
 
     @Override
     public StudentDto getStudent(String StudentId, Connection connection) {
-        return null;
-    }
+        StudentDto studentDto = new StudentDto();
 
+        try{
+            var ps = connection.prepareStatement(GET_STUDENT);
+            ps.setString(1, StudentId);
+            var resultSet = ps.executeQuery();
+            while(resultSet.next()){
+                studentDto.setId(resultSet.getString("id"));
+                studentDto.setName(resultSet.getString("name"));
+                studentDto.setEmail(resultSet.getString("email"));
+                studentDto.setCity(resultSet.getString("city"));
+                studentDto.setLevel(resultSet.getString("level"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return studentDto;
+    }
     @Override
     public boolean saveStudent(StudentDto student, Connection connection) {
         return false;
