58a4a12f4f53242c60346fc37e1a33f0e043b6f4
Sam1ndaFernando <uthpala2001@gmail.com>
2024-07-16 22:12:46 +0530

    getter setters added

--- src/main/java/dto/StudentDto.java
+++ src/main/java/dto/StudentDto.java
@@ -1,14 +1,14 @@
 package dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Getter
+@Setter
 public class StudentDto implements Serializable {
     private String id;
     private String name;
